c5b5bb2f0b43bd7310ccb0f5676ef7c57536bd5e
Nancy Chaudhry <nancych14@gmail.com>
2018-11-29 13:43:12 +0300

    Added a plugin and created case study page

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -15,3 +15,21 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+//Custom Post Type
+//Creat a case study custom post sqlite_fetch_column_type
+
+function create_custom_post_types() {
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ),
+        )
+    );
+}
+add_action( 'init', 'create_custom_post_types' );
